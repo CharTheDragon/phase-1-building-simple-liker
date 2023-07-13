ae3185a0736f709bfcf9069b1219218cf3d914c1
Nathaniel Swan <cygnet1.999@gmail.com>
2023-07-12 21:55:46 -0500

    that was longer than i thought

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let hearts = document.querySelectorAll('.like-glyph')
 
-// Your JavaScript code goes here!
+function addErrorMessage(){
+  let hiddenTag = document.querySelector('#modal')
+  hiddenTag.classList.remove('hidden')
+}
+
+function removeErrorMessage(){
+  let hiddenTag = document.querySelector('#modal')
+  hiddenTag.classList.add('hidden')
+}
+
+function checkHeart(event) {
+  if (event.target.innerText === FULL_HEART) {
+    console.log(event.target.classList)
+    event.target.innerText = EMPTY_HEART;
+    event.target.classList.remove('activated-heart');
+  } else {
+    mimicServerCall()
+    .then(function() {
+    console.log(event.target)
+    event.target.classList.add('activated-heart');
+    event.target.innerText = FULL_HEART;
+    })
+    .catch(function() {
+    addErrorMessage();
+    setTimeout(function(){
+      removeErrorMessage()},3000);
+    console.log('catch')
+    })}
+
+}
+
+function heartClicked() {
+  for (item of hearts) {
+    item.addEventListener('click',checkHeart)
+  } 
+}
+
+heartClicked()
 
 
-
+//call server within another function
+//if callsback as goo, change the heart
+//create catch that changes modal
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
